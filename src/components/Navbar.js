a5311fbb8d8522f138b2976fc9ceeb53ca9e028e
Sathvik10 <sathvik.ghl@gmail.com>
2024-01-22 13:28:11 -0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,22 +3,22 @@
 import React from 'react';
 import '../styling/Navbar.css'; // Import a separate CSS file for styling
 
-
+const baseUrl = "/i_am_Kote"
 function Navbar() {
   return (
     <nav className="navbar flex justify-center bg-neutral-900/50 p-4 backdrop-blur gap-x-8">
       <ul className="nav-list text-white">
         <li className="nav-item">
-          <a href='/#home'>Home</a>
+          <a href='/i_am_Kote/#home'>Home</a>
         </li>
         <li className="nav-item">
-          <a href='/#about'>About Me</a>
+          <a href='/i_am_Kote/#about'>About Me</a>
         </li>
         <li className="nav-item">
-          <a href='/#resume'>Resume</a>
+          <a href='/i_am_Kote/#resume'>Resume</a>
         </li>
         <li className="nav-item">
-          <a href='/#project'>Projects</a>
+          <a href='/i_am_Kote/#project'>Projects</a>
         </li>
       </ul>
     </nav>
